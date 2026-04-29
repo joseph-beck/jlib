4ace9595cf851df5f3f1b797faa81df7e664c14f
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-04-29 11:07:13 +0100

    docs: update docs for should

--- packages/should/src/should-never.ts
+++ packages/should/src/should-never.ts
@@ -1,3 +1,10 @@
+/**
+ * shouldNever always throws an error when called.
+ * This is useful for failing in areas that should never be reached.
+ *
+ * @param message custom error message to throw if the value is called
+ * @throws {Error} if the value is called
+ */
 const shouldNever = (message?: string): never => {
   throw new Error(message ?? 'should never be called')
 }
